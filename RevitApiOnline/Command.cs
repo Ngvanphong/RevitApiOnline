983057d3efb9ce7dc6a9e2ab33ecf1bdea0ffc35
Admin <ngvanphong2012@gmail.com>
2025-05-24 21:35:38 +0700

    Add project files.

---
+++ RevitApiOnline/Command.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+//using winform = System.Windows.Forms;
+
+namespace RevitApiOnline
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Succes", "This is First Addin");
+            //winform.TaskDialog()
+
+            return Result.Succeeded;
+        }
+    }
+}
