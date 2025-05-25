891e5d061736c7f55ec3aa360ae11fe72276df2d
Nguyen van Phong <ngvanphong2012@gmail.com>
2025-05-25 10:11:58 +0700

    52525

--- RevitApiOnline/Command.cs
+++ RevitApiOnline/Command.cs
@@ -1,7 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
-using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-//using winform = System.Windows.Forms;
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Windows.Media.Animation;
 
 namespace RevitApiOnline
 {
@@ -10,8 +11,42 @@ namespace RevitApiOnline
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Success", "This is First Addin");
-            //winform.TaskDialog()
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+            ICollection<ElementId> ids= uiDoc.Selection.GetElementIds();
+            List<Curve> listCurveWall= new List<Curve>();
+            foreach(ElementId id in ids)
+            {
+                Element element= doc.GetElement(id);
+                bool isDetail = element is DetailCurve;
+                if (isDetail)
+                {
+                    DetailCurve detailCurve= element as DetailCurve;
+                    Curve curve= detailCurve.GeometryCurve;
+                    listCurveWall.Add(curve);
+                }
+            }
+
+            View view = doc.ActiveView;
+            Level level= view.GenLevel;
+
+            Parameter levelParameter = view.get_Parameter(BuiltInParameter.PLAN_VIEW_LEVEL);
+            string levelName = levelParameter.AsString();
+            
+            using(Transaction t= new Transaction(doc, "CreateWall"))
+            {
+                t.Start();
+                foreach(Curve curve in listCurveWall)
+                {
+                   Wall wall= Wall.Create(doc, curve, level.Id, false);
+                }
+                t.Commit();
+            }
+            
+
+
+            
+           
 
             return Result.Succeeded;
         }
