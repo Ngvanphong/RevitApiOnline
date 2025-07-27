ea3497f85f38731d23601aba726e855f0cea2650
Nguyen van Phong <ngvanphong2012@gmail.com>
2025-07-27 10:35:05 +0700

    5252525

--- RevitApiOnline/App.cs
+++ RevitApiOnline/App.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.UI;
+using RevitApiOnline.Buttons;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,11 @@ namespace RevitApiOnline
     {
         public Result OnStartup(UIControlledApplication application)
         {
+            new CreatePipeButton().CreatePipe(application);
+            new DemoSplitButton().DemoSplit(application);
+            new DemoPulldownButton().DemoPulldown(application);
+            new DemoSmallButton().DemoSmall(application);
+            new DemoCombineButton().DemoCombine(application);
             return Result.Succeeded;
         }
         public Result OnShutdown(UIControlledApplication application)
---
+++ RevitApiOnline/Buttons/CreatePipeButton.cs
@@ -0,0 +1,57 @@
+﻿using Autodesk.Revit.UI;
+using RevitApiOnline.CreatePiping;
+using System;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using UIFramework;
+using adwindow = Autodesk.Windows;
+
+namespace RevitApiOnline.Buttons
+{
+    public class CreatePipeButton
+    {
+        public void CreatePipe(UIControlledApplication app)
+        {
+            adwindow.RibbonTab ribbonTab = null;
+            adwindow.RibbonControl ribbonControl = RevitRibbonControl.RibbonControl;
+            foreach (var tab in ribbonControl.Tabs)
+            {
+                if (tab.AutomationName == AppConstants.TabName)
+                {
+                    ribbonTab = tab;
+                    break;
+                }
+            }
+            if (ribbonTab == null)
+            {
+                //create ribbon tab
+                app.CreateRibbonTab(AppConstants.TabName);
+            }
+
+            RibbonPanel ribbonPanel = null;
+            foreach (RibbonPanel panel in app.GetRibbonPanels(AppConstants.TabName))
+            {
+                if (panel.Name == AppConstants.PanelName)
+                {
+                    ribbonPanel = panel;
+                    break;
+                }
+            }
+
+            if (ribbonPanel == null)
+            {
+                ribbonPanel = app.CreateRibbonPanel(AppConstants.TabName, AppConstants.PanelName);
+            }
+
+            string buttonId = $"CreatePipe{new Random().Next(1, 9000).ToString()}";
+            PushButtonData pushButtonData = new PushButtonData(buttonId, "Create\nPiping",
+                Assembly.GetExecutingAssembly().Location, typeof(CreatePipeBinding).FullName);
+            pushButtonData.LongDescription = "Long Desciption";
+            pushButtonData.Image = new BitmapImage(new Uri("/RevitApiOnline;component/Images/icons8crop24.png", UriKind.RelativeOrAbsolute));
+            pushButtonData.LargeImage = new BitmapImage(new Uri("/RevitApiOnline;component/Images/icons8crop24.png", UriKind.RelativeOrAbsolute));
+
+            PushButton pushButton = ribbonPanel.AddItem(pushButtonData) as PushButton;
+            pushButton.Enabled = true;
+        }
+    }
+}
