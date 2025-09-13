afff3ccff176efacc658d8a93a56f14af5867d43
unknown <ngvanphong2012@gmail.com>
2025-09-13 19:57:16 +0700

    edit

--- RevitApiOnline/CreatePiping/CreatePipeBinding.cs
+++ RevitApiOnline/CreatePiping/CreatePipeBinding.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using RevitApiOnline.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,19 @@ namespace RevitApiOnline.CreatePiping
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc= uiDoc.Document;
+            var ids= uiDoc.Selection.GetElementIds();
+            if (ids == null) return Result.Succeeded;
+            foreach(ElementId id in ids)
+            {
+                Element element= doc.GetElement(id);
+                List<Solid> listSolid = new List<Solid>();
+                GeometryHelper.GeoSolidElement(doc, element,ref listSolid);
+                List<Reference> listHorizotalRef = new List<Reference>();
+                List<Reference> listVerticalRef = new List<Reference>();
+
+            }
             return Result.Succeeded;
         }
     }
---
+++ RevitApiOnline/Shared/GeometryHelper.cs
@@ -0,0 +1,70 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitApiOnline.Shared
+{
+    public class GeometryHelper
+    {
+        public static void GeoSolidElement(Document doc,Element element,ref List<Solid> listSolid)
+        {
+            Options options= new Options();
+            options.ComputeReferences = true;
+            options.IncludeNonVisibleObjects = false;
+            options.View = doc.ActiveView;
+
+            //Get Solid of this element
+            GeometryElement geoElement = element.get_Geometry(options);
+            foreach (GeometryObject geoObj in geoElement)
+            {
+                if(geoObj is Solid)
+                {
+                    Solid solid = (Solid)geoObj;
+                    if (solid.Volume > 0.000000001)
+                    {
+                        listSolid.Add(solid);   
+                    }
+                }
+                else if(geoObj is GeometryInstance)
+                {
+                    GeometryInstance geoInstance= (GeometryInstance)geoObj;
+                    if (geoInstance.GetInstanceGeometry() != null)
+                    {
+                        foreach (GeometryObject geoObj2 in geoInstance.GetInstanceGeometry())
+                        {
+                            if (geoObj2 is Solid)
+                            {
+                                Solid solid = (Solid)geoObj2;
+                                if (solid.Volume > 0.000000001)
+                                {
+                                    listSolid.Add(solid);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //Get Solid of SubComponnet;
+            if(element is FamilyInstance)
+            {
+                FamilyInstance instance= (FamilyInstance)element;
+                var subIds= instance.GetSubComponentIds();
+                if (subIds != null)
+                {
+                    foreach (var subId in subIds)
+                    {
+                        Element elementSub = doc.GetElement(subId);
+                        GeoSolidElement(doc, elementSub, ref listSolid);
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
